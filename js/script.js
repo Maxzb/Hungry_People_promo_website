1bc128093f6b7061396292242def0656b0b7e295
MaxZb <47795883+Maxzb@users.noreply.github.com>
2020-07-25 02:46:17 +0300

    Slider-3

--- js/script.js
+++ js/script.js
@@ -6,19 +6,19 @@ $(document).ready(function () {
 		$('body').toggleClass('scroll');
 	});
 
-	
+
 
 	// SLICK-SLIDER
 
 	$(".specialties-slider").slick({
-		arrows:false,
-		dots:true,
-		slidesToShow:1,
+		arrows: false,
+		dots: true,
+		slidesToShow: 1,
 		slidesToScroll: 1,
 		adaptiveHeight: true,
 		autoplay: true,
 		autoplaySpeed: 3500,
-		appendDots:$('.specialties-slider_dots')
+		appendDots: $('.specialties-slider_dots')
 		/* speed:1000, */
 		/* responsive:[
 			{
@@ -37,23 +37,29 @@ $(document).ready(function () {
 	});
 
 	//ПЛАВНОЕ ПЕРЕМЕЩЕНИЕ К ЯКОРЯМ
-	/* $(function(){
-		$('.header-block__nav, .nav__list--footer-column').on("click","a", function (event) {
+	$(function () {
+		$('.menu').on("click", "a", function (event) {
 			event.preventDefault();
 			//забираем идентификатор с атрибута href
-			var id  = $(this).attr('href'),
-			//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).position().top;
+			var id = $(this).attr('href'),
+				//узнаем высоту от начала страницы до блока на который ссылается якорь
+				top = $(id).position().top;
 			//анимируем переход на расстояние
-			$('body,html').animate({scrollTop: top}, 1000);
-		}); */
+			$('body, html').animate({scrollTop: top}, 600);
+			$('.hamburger').toggleClass('is-active');
+			$('.mob-menu').toggleClass('menu-active');
+			$('.mob-menu__circle').toggleClass('mob-menu__circle--transform');
+			$('body').toggleClass('scroll');
+
+		});
+	});
 
 	// МАСКА ДЛЯ ТЕЛЕФОНА
-	/* $(function () {
-		$("#phone").mask('+7 (999) 999-9999', {
+	$(function () {
+		$("#tel").mask('+40 999 999 999', {
 			autoclear: false
 		});
-	}); */
+	});
 
 	// СТИЛИЗАЦИЯ СКРОЛА
 	/* $(".inner-block__order-list").mCustomScrollbar({}); */
